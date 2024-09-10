af582f0abf47491f14d6290faf45dac44f5d9ed9
gitjiho <jiho99322@gmail.com>
2024-09-10 21:18:47 +0900

    init: set project

---
+++ src/main/java/com/knu/daeguhackathon/DaeguhackathonApplication.java
@@ -0,0 +1,13 @@
+package com.knu.daeguhackathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaeguhackathonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaeguhackathonApplication.class, args);
+	}
+
+}
