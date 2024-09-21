172ae07af8e230a0226dfb11f0182f7c0978d72f
geon <kky8586696@naver.com>
2024-09-21 18:14:22 +0900

    Feat: 검색기록 조회 삭제 모두 삭제 구현

--- src/main/java/com/knu/daeguhackathon/searchlog/controller/SearchLogController.java
+++ src/main/java/com/knu/daeguhackathon/searchlog/controller/SearchLogController.java
@@ -1,10 +1,40 @@
 package com.knu.daeguhackathon.searchlog.controller;
 
+import com.knu.daeguhackathon.global.dto.GlobalResponse;
+import com.knu.daeguhackathon.searchlog.controller.dto.SearchLogResponse;
+import com.knu.daeguhackathon.searchlog.service.SearchLogService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/searchlog")
 public class SearchLogController {
+    private final SearchLogService searchLogService;
+
+    @GetMapping("")
+    public SearchLogResponse.Logs getAllSearchLogs(){
+        // TODO getAllSearchLogs()
+        return null;
+
+    }
+
+    @DeleteMapping("/{logId}")
+    public GlobalResponse deleteSearchLog(
+            @RequestParam(name = "logId") Long logId
+    ){
+        searchLogService.deleteSearchLogById(logId);
+        return GlobalResponse.builder()
+                .message("로그 삭제가 완료되었습니다.")
+                .build();
+    }
+    @DeleteMapping("/all")
+    public GlobalResponse deleteAllSearchLog(
+    ){
+        //TODO searchLogService.deleteAllSearchLog()
+        return GlobalResponse.builder()
+                .message("전체 로그 삭제가 완료되었습니다.")
+                .build();
+    }
 }
