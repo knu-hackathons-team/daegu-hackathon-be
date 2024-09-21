1657a359f7d68f99d8c6d07990052b9aae1a1773
geon <kky8586696@naver.com>
2024-09-21 23:45:25 +0900

    Feat: 시간표 데이터베이스 추출

---
+++ src/main/java/com/knu/daeguhackathon/global/utils/DataLoader.java
@@ -0,0 +1,46 @@
+package com.knu.daeguhackathon.global.utils;
+
+import com.knu.daeguhackathon.global.utils.course.Course;
+import com.knu.daeguhackathon.global.utils.course.repository.CourseRepository;
+import com.opencsv.CSVReader;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.io.FileReader;
+
+@Component
+public class DataLoader implements CommandLineRunner {
+
+    private final CourseRepository courseRepository;
+
+    public DataLoader(CourseRepository courseRepository) {
+        this.courseRepository = courseRepository;
+    }
+
+    @Override
+    public void run(String... args) {
+        importCsv("csvfile.csv");  // CSV 파일 경로
+    }
+
+    private void importCsv(String filePath) {
+        try (CSVReader csvReader = new CSVReader(new FileReader(filePath))) {
+            String[] nextLine;
+            csvReader.readNext();
+            csvReader.readNext();
+            csvReader.readNext();
+            while ((nextLine = csvReader.readNext()) != null) {
+                if(!nextLine[8].contains("상주캠퍼스")){// 강좌번호
+                    Course course = new Course();
+                    course.setCourseId(nextLine[0]);
+                    course.setCourseName(nextLine[4]);    // 교과목명
+                    course.setType(nextLine[6]);           // 구분
+                    course.setLectureTime(nextLine[7]);    // 강의시간
+                    course.setClassroom(nextLine[8]);      // 강의실
+                    courseRepository.save(course);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
