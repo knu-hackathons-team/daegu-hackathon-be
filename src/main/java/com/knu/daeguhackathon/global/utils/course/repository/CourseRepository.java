4872581e9c84d79f40d92b5f77df23d0575d1f57
gitjiho <jiho99322@gmail.com>
2024-09-22 14:18:35 +0900

    feat: 시간표 검색 로직 추가

--- src/main/java/com/knu/daeguhackathon/global/utils/course/repository/CourseRepository.java
+++ src/main/java/com/knu/daeguhackathon/global/utils/course/repository/CourseRepository.java
@@ -1,10 +1,13 @@
 package com.knu.daeguhackathon.global.utils.course.repository;
 
 import com.knu.daeguhackathon.global.utils.course.Course;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CourseRepository extends JpaRepository<Course, Long> {
+
+    List<Course> findAllByCourseName(String name);
 
 }
