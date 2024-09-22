f9cca7a715b2c60f01cde05e76f34667445eb1a8
JIHO LEE <161289673+GitJIHO@users.noreply.github.com>
2024-09-22 17:56:42 +0900

    fix: 카카오메시지 자료형 수정 (#35)

--- src/main/java/com/knu/daeguhackathon/kakaotoken/dto/KakaoResultRequest.java
+++ src/main/java/com/knu/daeguhackathon/kakaotoken/dto/KakaoResultRequest.java
@@ -1,6 +1,6 @@
 package com.knu.daeguhackathon.kakaotoken.dto;
 public record KakaoResultRequest(
-    String start, String end, int estimatedTime, Double buildingDistance
+    String start, String end, Double estimatedTime, Double buildingDistance
 ) {
 
 }
