1ee7ef3436098e80c10fbf8b62cb134db1811e07
gitjiho <jiho99322@gmail.com>
2024-10-19 17:34:42 +0900

    fix: bug 수정

--- src/main/java/com/knu/daeguhackathon/member/service/MemberService.java
+++ src/main/java/com/knu/daeguhackathon/member/service/MemberService.java
@@ -51,7 +51,7 @@ public class MemberService {
 
         return MemberResponse.builder()
             .name(member.getName())
-            .nickName(member.getName())
+            .nickName(member.getNickName())
             .email(member.getEmail())
             .createdTime(member.getCreatedTime())
             .build();
